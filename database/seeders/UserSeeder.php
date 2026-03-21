bbbd7df306e1201ee0352024726b812a94c1bf40
Luqman89 <luqmnhakim3@gmail.com>
2026-03-21 18:16:47 +0700

    feat: implement role management using spatie permission with role and user seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // assign role dari Spatie
+        $admin->assignRole('admin');
+    }
+}
